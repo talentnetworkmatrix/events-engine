f59604f7a33ea484a2eff2bc218744e7a25be06a
Diego Figueroa <diego.figueroa@talent-network.org>
2023-02-28 16:42:20 -0600

    Chore: Update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
